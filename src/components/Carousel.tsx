82ae6ba7a0b1ef318de615dc71309611326935d5
Ain Afridi <afridi.ainulhaq@gmail.com>
2024-11-22 11:27:45 +0500

    design ready

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -6,22 +6,31 @@ import { MyContext } from '../context/provider'
 
 const Carousel = () => {
 
-  const {quiz, quizIndex } = useContext(MyContext)!;
-  
+  const { quiz, quizIndex, setQuizIndex } = useContext(MyContext)!;
+
 
   return (
     <div className="container-carousal flex">
-      
-      <div className='mobile-screen customPurple relative'>
-      <div className='absolute left-10 top-10 book-image'>
-        <img src={assets.book} alt='book' width={30} height={30} />
-      </div>
 
-      <div className='absolute left-10 top-[50%] translate-y-[-50%] flex flex-col gap-3'>
-        {
-          quiz.map((value, i)=> <div key={i} className={`w-[13px] h-[13px] mobile-dot rounded-[50%] ${(value.answer || i <= quizIndex) ? 'border-[#fff] border-[2px]' : 'bg-white'}`}></div>)
-        }
-      </div>
+      <div className='mobile-screen customPurple relative'>
+        <div className='absolute left-10 top-10 book-image'>
+          <img src={assets.book} alt='book' width={30} height={30} />
+        </div>
+
+        <div className='absolute left-10 top-[50%] translate-y-[-50%] flex flex-col gap-3'>
+          {
+            quiz.map((value, i) =>
+              <div
+                key={i}
+                className={`w-[13px] cursor-pointer h-[13px] mobile-dot rounded-[50%] ${(value.answer || i <= quizIndex) ? 'border-[#fff] border-[2px]' : 'bg-white'}`}
+                onClick={()=> {
+                  if(value.answer){
+                    setQuizIndex(i)
+                  }
+                }}
+              ></div>)
+          }
+        </div>
         <LeftSide />
       </div>
       <div className='mobile-screen'>
